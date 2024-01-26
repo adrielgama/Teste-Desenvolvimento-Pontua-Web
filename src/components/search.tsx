c2209d43fcd20df32d76dcce6952264c8d5795f5
Adriel Gama <adrielgama@gmail.com>
2024-01-25 22:34:20 -0300

    fix: adjustments to view items

--- src/components/search.tsx
+++ src/components/search.tsx
@@ -4,7 +4,7 @@ import { Input } from './ui/input'
 
 function Search() {
   return (
-    <div className="flex w-screen items-center justify-center gap-0 bg-white p-4 font-navigation font-medium text-blue-200 shadow-menu-shadow lg:fixed lg:left-64">
+    <div className="fixed flex w-full items-center justify-center gap-0 bg-white p-4 font-navigation font-medium text-blue-200 shadow-menu-shadow lg:fixed lg:left-64 lg:pb-[15px] lg:pl-8">
       <SearchIcon size={15} className="font-medium" />
       <Input
         className="border-none focus-visible:ring-transparent"
--- src/components/sidenav.tsx
+++ src/components/sidenav.tsx
@@ -12,7 +12,7 @@ function Sidenav() {
   const location = useLocation()
 
   return (
-    <div className="fixed bottom-0 z-50 flex h-16 w-full items-center justify-evenly shadow-menu-shadow lg:h-screen lg:w-64 lg:flex-col lg:items-start lg:justify-normal lg:space-y-2">
+    <div className="fixed bottom-0 z-50 flex h-16 w-full items-center justify-evenly bg-white shadow-menu-shadow lg:h-screen lg:w-64 lg:flex-col lg:items-start lg:justify-normal lg:space-y-2">
       <img
         src={LogoPontuaBlue}
         alt="Logo PONTUA cor azul"
@@ -24,7 +24,7 @@ function Sidenav() {
         <ul key={index} className="flex w-full flex-col gap-2 px-3">
           <li
             className={`flex cursor-pointer flex-row items-center justify-center gap-3 rounded-lg p-2 text-[13px] transition-all ${
-              location.pathname === item.route ? 'bg-[#F5F6F8]' : ''
+              location.pathname === item.route ? 'text-orange-500' : ''
             } font-medium hover:bg-gray-100 lg:justify-normal`}
           >
             {React.createElement(item.icon, { size: 20 })} {item.label}
