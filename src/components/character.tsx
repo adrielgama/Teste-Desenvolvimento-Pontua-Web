26763c2092e37dbe773b76838be062859f98e3a4
Adriel Gama <adrielgama@gmail.com>
2024-02-02 12:38:06 -0300

    style: add skeleton items

--- src/components/character.tsx
+++ src/components/character.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 import NotAvailableIMG from '@/assets/image-not-available.webp'
 
+import { Skeleton } from './ui/skeleton'
+
 interface CharacterProps {
   name: string
   description: string
@@ -13,26 +15,54 @@ export const Character: React.FC<CharacterProps> = ({
   description,
   image,
 }) => {
+  const [isLoading, setIsLoading] = React.useState(true)
   const isNotImageAvailable =
     image ===
     'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg'
 
+  React.useEffect(() => {
+    const imageElement = new Image()
+    imageElement.src = image
+
+    imageElement.onload = () => {
+      setIsLoading(false)
+    }
+  }, [image])
+
   return (
     <div className="flex cursor-pointer flex-row gap-4 rounded-2xl bg-gray-100 p-4 font-navigation transition-all lg:opacity-80 lg:hover:opacity-100">
-      <img
-        src={isNotImageAvailable ? NotAvailableIMG : image}
-        alt={isNotImageAvailable ? 'Not Available' : `${name} thumbnail`}
-        className={`h-32 w-20 rounded-xl bg-white  ${isNotImageAvailable ? 'object-contain' : 'object-cover'}`}
-      />
+      {isLoading ? (
+        <Skeleton className="mb-4 h-28 w-20 rounded-xl" />
+      ) : (
+        <img
+          src={isNotImageAvailable ? NotAvailableIMG : image}
+          alt={isNotImageAvailable ? 'Not Available' : `${name} thumbnail`}
+          className={`h-32 w-20 rounded-xl bg-white ${
+            isNotImageAvailable ? 'object-contain' : 'object-cover'
+          }`}
+        />
+      )}
       <div className="flex h-full flex-col gap-2">
-        <h1 className="font-bold" aria-label={name}>
-          {name}
-        </h1>
-        <p
-          className={`${description ? '' : 'italic text-gray-400'} line-clamp-4 text-xs`}
-        >
-          {description || 'Character without description'}
-        </p>
+        {isLoading ? (
+          <>
+            <Skeleton className="mb-2 h-6 w-48" />
+            <Skeleton className="h-3 w-64" />
+            <Skeleton className="h-3 w-56" />
+            <Skeleton className="h-3 w-52" />
+            <Skeleton className="h-3 w-56" />
+          </>
+        ) : (
+          <>
+            <h1 className="font-bold" aria-label={name}>
+              {name}
+            </h1>
+            <p
+              className={`${description ? '' : 'italic text-gray-400'} line-clamp-4 text-xs`}
+            >
+              {description || 'Character without description'}
+            </p>
+          </>
+        )}
       </div>
     </div>
   )
--- src/components/profile/profileBoxCharacter.tsx
+++ src/components/profile/profileBoxCharacter.tsx
@@ -4,6 +4,8 @@ import NotAvailableIMG from '@/assets/image-not-available.webp'
 import { Avatar, AvatarImage, AvatarFallback } from '@/components/ui/avatar'
 import { ScrollArea } from '@/components/ui/scroll-area'
 
+import { Skeleton } from '../ui/skeleton'
+
 interface CharacterProps {
   name: string
   description: string
@@ -15,33 +17,69 @@ const ProfileBoxCharacter: React.FC<CharacterProps> = ({
   description,
   image,
 }) => {
+  const [isLoading, setIsLoading] = React.useState(true)
   const isNotImageAvailable =
     image ===
     'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg'
 
+  React.useEffect(() => {
+    const imageElement = new Image()
+    imageElement.src = image
+
+    imageElement.onload = () => {
+      setIsLoading(false)
+    }
+  }, [image])
+
   return (
-    <div className="shadow-profile-shadow mt-7 flex w-full flex-col gap-4 rounded-2xl bg-white px-8 py-10 lg:h-64 lg:flex-row lg:px-10 xl:h-56">
+    <div className="mt-7 flex w-full flex-col gap-4 rounded-2xl bg-white px-8 py-10 shadow-profile-shadow lg:h-64 lg:flex-row lg:px-10 xl:h-56">
       <div className="flex items-center gap-7 lg:items-start">
-        <Avatar className="h-24 w-24">
-          <AvatarImage
-            src={isNotImageAvailable ? NotAvailableIMG : image}
-            alt={isNotImageAvailable ? 'Not Available' : `${name} thumbnail`}
-          />
-          <AvatarFallback>{name[0]}</AvatarFallback>
-        </Avatar>
-        <h1 className="text-2xl font-bold text-blue-600 lg:hidden">{name}</h1>
+        {isLoading ? (
+          <Skeleton className="h-24 w-24 rounded-full" />
+        ) : (
+          <Avatar className="h-24 w-24">
+            <AvatarImage
+              src={isNotImageAvailable ? NotAvailableIMG : image}
+              alt={isNotImageAvailable ? 'Not Available' : `${name} thumbnail`}
+            />
+            <AvatarFallback>{name[0]}</AvatarFallback>
+          </Avatar>
+        )}
+        {isLoading ? (
+          <div className="lg:hidden">
+            <Skeleton className="mb-4 h-6 w-48" />
+            <Skeleton className="mb-2 h-2 w-48" />
+            <Skeleton className="mb-2 h-2 w-32" />
+            <Skeleton className="mb-2 h-2 w-40" />
+          </div>
+        ) : (
+          <h1 className="text-2xl font-bold text-blue-600 lg:hidden">{name}</h1>
+        )}
       </div>
       <div className="flex w-full flex-col gap-4">
-        <h1 className="hidden text-2xl font-bold text-blue-600 lg:block">
-          {name}
-        </h1>
-        <ScrollArea className="h-48 xl:h-full">
-          <p
-            className={`${description ? 'font-semibold' : 'font-normal italic text-gray-400/90'} leading-6 text-[rgb(113,113,113)]`}
-          >
-            {description || 'Character without description'}
-          </p>
-        </ScrollArea>
+        {isLoading ? (
+          <div className="hidden lg:block">
+            <Skeleton className="mb-4 h-6 w-48" />
+            <Skeleton className="mb-2 h-2 max-w-screen-lg" />
+            <Skeleton className="mb-2 h-2 max-w-screen-md" />
+            <Skeleton className="mb-2 h-2 max-w-screen-lg" />
+            <Skeleton className="mb-2 h-2 max-w-screen-sm" />
+            <Skeleton className="mb-2 h-2 max-w-screen-2xl" />
+          </div>
+        ) : (
+          <>
+            <h1 className="hidden text-2xl font-bold text-blue-600 lg:block">
+              {name}
+            </h1>
+            <ScrollArea className="h-48 xl:h-full">
+              <p
+                className={`${description ? 'font-semibold' : 'font-normal italic text-gray-400/90'} leading-6 text-[rgb(113,113,113)]`}
+              >
+                {description || 'Character without description'}
+              </p>
+            </ScrollArea>
+          </>
+        )}
       </div>
     </div>
   )
